b033e4800a9d8c05ab2a66eaa13bb07e4a5a0842
Gary Behan <gary.b@browserstack.com>
2023-05-31 14:29:02 +0100

    Removing extraneous lines

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -1,10 +1,6 @@
-const LoginPage = require('../pageobjects/login.page')
-const SecurePage = require('../pageobjects/secure.page')
-
 describe('Simple Geolocation Testing Using Chrome Modheader', () => {
     it('should redirect to the https://fr.norton.com', async () => {
-        //await LoginPage.open();
-        //console.log('');
+
         await browser.pause(5000);
         await browser.url('https://norton.com');
         await browser.pause(5000);
